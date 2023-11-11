24ca0aca009e91af1d29633218a395a59b34a399
zk <kustovweb@gmail.com>
2023-11-11 15:35:13 +0700

    seminar 3

---
+++ src/main/java/sem3/coverage/SomeService.java
@@ -0,0 +1,15 @@
+package sem3.coverage;
+
+public class SomeService {
+    // Метод возвращает Fizz для чисел кратных 3, Buzz для кратных 5, и FizzBuzz для кратных 3 и 5 одновременно
+
+    public String fizzBuzz(int i) {
+        return null;
+    }
+
+    // Метод возвращает true для массивов, которые начинаются или заканчиваются 6, и false - если 6 нет ни в начале ни в конце массива
+    public boolean firstLast6(int[] nums) {
+        return false;
+    }
+
+}
